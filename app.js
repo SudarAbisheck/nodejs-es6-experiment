05dd0999f840d9baeaf726daa5c5eba65d3bc20f
Sudar Abisheck <sudarabisheck@gmail.com>
2015-09-22 07:39:06 +0530

    ejs added successfully

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var _express2 = _interopRequireDefault(_express);
 
 var app = (0, _express2["default"])();
 
+app.set("port", process.env.PORT || 5000);
+
+app.use(_express2["default"]["static"](__dirname + '/public'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.use(function (req, res, next) {
     console.log("middleware hoo !!!!");
     next();
 });
 app.get("/", function (req, res) {
-    res.send("root route working");
+    res.render("pages/index");
 });
 
+app.get("/redirect", function (req, res) {
+    return res.redirect("http://www.sudar.me");
+});
 app.get('/details/:id/:name', function (req, res, next) {
     if (req.params.id == 234) next("route");
     res.json("{\"id\" : " + req.params.id + ", \"name\": \"" + req.params.name + "\", \"msg\": \"First get caught the request\"}");
@@ -25,7 +34,7 @@ app.get("/details/:id/:name", function (req, res) {
     return res.send("Details:  " + req.params.id + " " + req.params.name);
 });
 
-var server = app.listen(8005, function () {
-    console.log("Server running at port 8005");
+var server = app.listen(app.get("port"), function () {
+    console.log("Server running at port " + app.get("port"));
 });
 //# sourceMappingURL=app.js.map
--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 let app = express();
 
+app.set("port",(process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+app.set("views", __dirname + "/views");
+app.set("view engine" ,"ejs");
+
 app.use((req,res,next)=> {
     console.log("middleware hoo !!!!");
     next();
 });
 app.get("/",(req,res)=>{
-    res.send("root route working");
+    res.render("pages/index");
 });
 
+app.get("/redirect",(req,res)=> res.redirect("http://www.sudar.me"));
 app.get('/details/:id/:name', (req,res,next)=> {
     if(req.params.id == 234) next("route");
     res.json(`{"id" : ${req.params.id}, "name": "${req.params.name}", "msg": "First get caught the request"}`);
@@ -16,6 +23,6 @@ app.get('/details/:id/:name', (req,res,next)=> {
 
 app.get("/details/:id/:name", (req,res)=> res.send("Details:  "+ req.params.id + " " + req.params.name));
 
-var server = app.listen(8005, ()=>{
-    console.log("Server running at port 8005");
+var server = app.listen(app.get("port"), ()=>{
+    console.log("Server running at port " + app.get("port"));
 })
